d12c7f19e1e5a68d2de6c0c574903f6ce1e476a3
Cit <shavinsept@gmail.com>
2022-08-18 17:29:18 +1000

    Added Fun Commands

---
+++ Fun Commands/Commands/lyrics.js
@@ -0,0 +1,40 @@
+const { ChatInputCommandInteraction, Client, EmbedBuilder, ApplicationCommandOptionType } = require("discord.js");
+const axios = require("axios").default;
+
+module.exports = {
+    name: "lyrics",
+    description: "Get lyrics for a song",
+    options: [
+        {
+            name: "title",
+            description: "Provide the title of the song",
+            type: ApplicationCommandOptionType.String,
+            required: true
+        }
+    ],
+    /**
+     * @param {ChatInputCommandInteraction} interaction
+     * @param {Client} client
+     */
+    async execute(interaction, client) {
+        const embed = new EmbedBuilder();
+        await interaction.deferReply();
+
+        await axios.get(`https://some-random-api.ml/lyrics?title=${interaction.options.getString("title")}`).then(async (response) => {
+            embed
+                .setColor("NotQuiteBlack")
+                .setAuthor({ name: response.data.title })
+                .setURL(response.data.links.genius)
+                .setThumbnail(response.data.thumbnail.genius)
+                .setFooter({ text: `Song By ${response.data.author}` })
+                .setDescription(`${response.data.lyrics.slice(0, 4096)}`);
+            
+            await interaction.editReply({ embeds: [embed] });
+        }).catch(() => {
+            embed
+                .setColor("Red")
+                .setDescription(`${client.data.rejected} Couldn't find any song with that title`);
+            return interaction.editReply({ embeds: [embed], ephemeral: true });
+        });
+    },
+};
---
+++ Fun Commands/README.md
@@ -0,0 +1,5 @@
+# Fun Commands
+These commands are for you to enjoy with. It includes an `animal fact` command, an `image manipulation` command and also a `lyrics` command. The animal fact command will find any fact about the animal of your choice. The image manipulation command will create a custom image based on the inputs you have given. The lyrics command takes in a song name as the input and will return the lyrics for the given song.
+
+## Dependencies
+- [axios](https://axios-http.com/) (`npm i axios`)
